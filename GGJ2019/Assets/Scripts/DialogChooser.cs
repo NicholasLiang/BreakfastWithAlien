2440743a2c760b4ae974a36f1795159646a3f6d0
Michael Pattie <mjpattie@gmail.com>
2019-01-26 10:47:39 -0800

    Added demo of dialog system.

---
+++ GGJ2019/Assets/Scripts/DialogChooser.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogChooser : MonoBehaviour {
+
+    [System.Serializable]
+    public class KeyValuePair{
+        public string Key;
+        public Response Response;
+    }
+
+    [System.Serializable]
+    public class Response{
+        public string Answer;
+        public bool ChoosePrompt;
+    }
+
+    public Text Output;
+    public List<string> Prompts;
+    public string defaultPrompt;
+    [SerializeField] public List<KeyValuePair> ResponseList;
+    Dictionary<string, Response> Responses;
+    public Button[] InitialButtons;
+    public Button[] AllButtons;
+
+    List<string> ActiveKey;
+
+	public void Start()
+	{
+        ActiveKey = new List<string>();
+        Responses = new Dictionary<string, Response>();
+        foreach(KeyValuePair kvp in ResponseList){
+            Responses.Add(kvp.Key, kvp.Response);
+        }
+        AllButtons = FindObjectsOfType<Button>();
+        ResetButtons();
+	}
+
+	public void AddToString(string seg)
+    {
+        ActiveKey.Add(seg);
+    }
+
+    public void BackWord(){
+        ActiveKey.RemoveAt(ActiveKey.Count - 1);
+    }
+
+    public void EvaluateKey(){
+        string key = "";
+        foreach(string str in ActiveKey){
+            key += str;
+        }
+        Response response = Responses[key];
+        string dialog = response.Answer;
+        if(response.ChoosePrompt){
+            dialog += " " + GetPrompt();
+        }
+        ActiveKey.Clear();
+        Output.text = dialog;
+
+        ResetButtons();
+    }
+
+    private string GetPrompt(){
+        if (Prompts.Count>0)
+        {
+            int index = Random.Range(0, Prompts.Count);
+            string res = Prompts[index];
+            Prompts.RemoveAt(index);
+            return res;
+        }else{
+            return defaultPrompt;
+        }
+    }
+
+    void ResetButtons(){
+        foreach (Button b in AllButtons)
+        {
+            b.gameObject.SetActive(false);
+        }
+        foreach (Button b in InitialButtons)
+        {
+            b.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ GGJ2019/Assets/Scripts/WordChoice.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WordChoice : MonoBehaviour {
+
+    DialogChooser dialog;
+    public string value;
+    public bool EndSentence;
+    public List<Button> NextWords;
+
+
+	// Use this for initialization
+	void Start () {
+        dialog = FindObjectOfType<Canvas>().GetComponent<DialogChooser>(); //Ugly hack but it's a jam.
+	}
+
+    public void AddWord(){
+        dialog.AddToString(value);
+        if(EndSentence){
+            dialog.EvaluateKey();
+        }else{
+            RevealNext();
+        }
+    }
+
+    //public void Back(){
+    //    dialog.BackWord();
+    //}
+
+    public void RevealNext(){
+        foreach(Button b in NextWords){
+            b.gameObject.SetActive(true);
+        }
+        gameObject.SetActive(false);
+    }
+
+    //public void RevealPrevious(){
+        
+    //}
+
+}
