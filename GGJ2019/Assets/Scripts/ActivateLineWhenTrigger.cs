b4eb64f69d38309f87abe56a73cf984a565036cf
Nicholas Liang <ningtakuyou@gmail.com>
2019-01-27 14:42:53 -0800

    Finalize the scene

---
+++ GGJ2019/Assets/Scripts/ActivateLineWhenTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateLineWhenTrigger : MonoBehaviour {
+    private LineRenderer line;
+
+    // Use this for initialization
+    void Start () {
+        line = GetComponent<LineRenderer>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (OVRInput.Get(OVRInput.Button.One) || OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            line.enabled = true;
+        }
+        else
+        {
+            line.enabled = false;
+        }
+	}
+}
